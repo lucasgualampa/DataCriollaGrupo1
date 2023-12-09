e972c38b7d59a28fcf7a8272ffdfb3123b0bf6b2
Brianlucas Gualampa <brianlucas.gualampa.contractor@gdevtools.com>
2023-12-09 20:28:42 -0300

    limpio codigo

--- stream.py
+++ stream.py
@@ -1,12 +1,3 @@
-import subprocess
-import sys
-import os
-#import pandas as pd
-
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#import plotly.express as px
-#import mysql.connector as connector
 import streamlit as st 
 import mysql.connector as connector
 
@@ -34,16 +25,13 @@ else:
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 
-# Reemplaza 'sqlite:///mi_basededatos.db' con tu cadena de conexión y tipo de base de datos
 engine = create_engine('mysql+mysqlconnector://root:root1234@localhost/olist_db')
 
-# Consultar los datos
 cursor = connection.cursor()
 cursor.execute("SELECT * FROM olist_sales_by_date_hour_city_category")
 
-# Obtener los resultados
 results = cursor.fetchall()
 
 st.dataframe(results)
 # Cerrar la conexión
-#connection.close()
+connection.close()
