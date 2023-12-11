507a371182dcafc26af0a2d41b41d66fe9ddc128
Brianlucas Gualampa <brianlucas.gualampa.contractor@gdevtools.com>
2023-12-11 00:15:36 -0300

    avance de graficos y ml

--- streamlit_app/pages/page_2.py
+++ streamlit_app/pages/page_2.py
@@ -2,6 +2,13 @@ import mysql
 import mysql.connector
 import streamlit as st
 import pandas as pd
+import plotly.graph_objects as go
+from datetime import datetime, timedelta
+import numpy as np
+import os
+from prophet.serialize import model_from_json
+from prophet import Prophet
+
 
 st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
@@ -62,3 +69,131 @@ cursor.execute("SELECT * FROM olist_order_items_dataset")
 order_items = cursor.fetchall()
 order_items_df = pd.DataFrame(order_items, columns=cursor.column_names)
 st.dataframe(order_items_df)
+
+
+""" # CARGA DEL MODELO #
+model_path = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "C:/Users/Lucas/Desktop/consultorBI/DataCriollaGrupo1/streamlit_app/pages/forecast_olis.json")
+model = model_from_json(open(model_path, 'r').read()) """
+
+
+# PREFICCION #
+
+
+def predicción(df_fechas):
+    df_pred = model.predict(df_fechas)
+    return df_pred
+
+
+def df_rango_fechas(start, end):
+    lista_fechas = [start + timedelta(days=d)
+                    for d in range((end - start).days+1)]
+    ds = pd.DataFrame({'ds': lista_fechas})
+    return ds
+
+# PLOTEO #
+
+
+def plot(df_predicted, df_real, ds):
+    """plotea serie de tiempo
+
+    Args:
+        df_predicted (datetime): dataframe con predicciones
+        df_real (datetime): dataframe con valores reales
+        ds (datetime): rango de fechas
+    """
+    #df_real = df_real[df_real.loc[ds['ds']]]
+    trace1 = go.Scatter(
+        x=df_real.index, y=df_real['y'], mode='markers', name='Real', marker=dict(color='mediumpurple'))
+    trace2 = go.Scatter(x=df_predicted['ds'], y=df_predicted['yhat'],
+                        mode='lines', name='Predicción', line=dict(color='red'))
+    fig = go.Figure(data=[trace1, trace2])
+    fig.update_layout(
+        xaxis_title='Fecha',
+        yaxis_title='Ventas')
+    # Show the figure
+    return fig
+
+# LAYOUT PRINCIPAL #
+def main():
+    # TITULOS
+    st.title('Forcasting de ventas :chart_with_upwards_trend:')
+    st.markdown('---')
+
+    # CARGA DEL DATAFRAME
+    with st.spinner('Extrayendo datos...'):
+        df_d = get_df_transformed()
+        connection.close()
+    st.success('Datos cargados exitosamente!')
+    st.markdown(""" Es importante analizar y entender la evolución y el comportamiento de 
+                los datos reales de venta a lo largo del tiempo. Por eso se presentan los componentes
+                de la serie a partir de la predicción correspondiente.""")
+
+    # SIDEBAR
+    with st.sidebar:
+        st.subheader('For our client:')
+        st.subheader('Made with :heart: by:')
+
+    st.header('Predicción con rango de fechas')
+
+    # Slicer
+    start_date = st.date_input(label="Fecha de inicio",
+                               value=datetime.strptime("2017-06-13", "%Y-%m-%d"))
+    end_date = st.date_input(label="Fecha de fin",
+                             value=datetime.strptime("2018-12-18", "%Y-%m-%d"))  # , format="Y-%m-%d"
+
+    if start_date < end_date:
+        st.success('Start date: `%s`\n\nEnd date:`%s`' %
+                   (start_date, end_date))
+        slider = st.slider(
+            'Selecciona el rango de fechas de predicción', min_value=start_date, value=(start_date, end_date))
+        st.write("Tu rango de predicción es:", slider)
+        # Dataframe de fechas
+
+        if st.button(label='Aplicar'):
+            ds = df_rango_fechas(slider[0], slider[1])
+            df_predict = model.predict(ds)
+            st.subheader("Predicción de venta diaria")
+            figura = plot(df_predict, df_d, ds)
+            st.plotly_chart(figura, sharing="streamlit", theme="streamlit")
+        else:
+            st.write('Click en el boton')
+
+    else:
+        st.error('Error: End date must fall after start date.')
+
+
+""" if __name__ == "__main__":
+    main() """
+
+cursor.execute("SELECT * FROM olist_orders_count_by_date")
+orders_by_date = cursor.fetchall()
+orders_by_date_df = pd.DataFrame(orders_by_date, columns=cursor.column_names)
+
+cursor.execute("SELECT * FROM olist_order_payments_dataset")
+payments_order = cursor.fetchall()
+payments_order_df = pd.DataFrame(payments_order, columns=cursor.column_names)
+
+items_agrupado = order_items_df.groupby(by=['order_id']).sum()
+items_agrupado['price'].sum()
+items_agrupado.shape
+items_agrupado.reset_index(inplace=True)
+items_agrupado.head()
+items_agrupado.duplicated(subset=['order_id'],keep=False).sum()
+
+payments_agrupado = payments_order_df.groupby(by='order_id').sum()
+payments_agrupado.reset_index(inplace=True)
+payments_agrupado.head()
+
+df_merge = orders_by_date_df.merge(payments_agrupado[['order_id','payment_value']], how = 'left', on='order_id').merge(items_agrupado[['order_id','price']], how = 'left', on='order_id')
+df_merge
+
+
+
+df_d = df_ts.resample('D').sum()
+# Instanciamos y entrenamos el modelo
+md = Prophet()
+#md.add_seasonality(name='monthly', period=30.5, fourier_order=5)
+md.fit(df_d.reset_index())
+
+fig = md.plot(forecast_d)
--- streamlit_app/stream.py
+++ streamlit_app/stream.py
@@ -1,9 +1,208 @@
-import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import sys
+import subprocess
+import plotly.graph_objects as go
+from prophet import Prophet
+from prophet.plot import add_changepoints_to_plot
+from prophet.diagnostics import cross_validation
+from prophet.plot import plot_cross_validation_metric
+from prophet.diagnostics import performance_metrics
+# Primero realizamos importaciones necesarios
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
+from statsmodels.tsa.seasonal import seasonal_decompose
+from pmdarima import auto_arima
+from sklearn.metrics import mean_squared_error
+from statsmodels.tools.eval_measures import rmse
+import warnings
+warnings.filterwarnings("ignore")
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+""" python_executable = sys.executable
+
+# El nombre del paquete que deseas instalar
+nombre_del_paquete = "prophet"
+
+# Utiliza subprocess para ejecutar el comando de instalación
+comando = [python_executable, "-m", "pip", "install", nombre_del_paquete]
+
+# Ejecuta el comando
+try:
+    subprocess.check_call(comando)
+    print(f"El paquete {nombre_del_paquete} se instaló correctamente.")
+except subprocess.CalledProcessError as e:
+    print(f"Error al instalar el paquete {nombre_del_paquete}.")
+    print(f"Error: {e}") """
+
+
+
+
+
+
+""" import streamlit as st
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
 
-st.write("""
-# Repositorio:
-https://github.com/lucasgualampa/DataCriollaGrupo1
-""")
+ """
+df_orders = pd.read_csv("C:/Users/Lucas/Desktop/consultorBI/DataCriollaGrupo1/streamlit_app/datasets/olist_orders_dataset.csv")
+#print(df_orders.info())
+
+""" 
+ 0   order_id                       96475 non-null  object
+ 1   customer_id                    96475 non-null  object
+ 2   order_status                   96475 non-null  object
+ 3   order_purchase_timestamp       96475 non-null  object 
+"""
+
+df_orders_items = pd.read_csv("C:/Users/Lucas/Desktop/consultorBI/DataCriollaGrupo1/streamlit_app/datasets/olist_order_items_dataset.csv")
+#print(df_orders_items.info())
+
+"""  
+5   price                112650 non-null  float64
+"""
+
+df_customers = pd.read_csv("C:/Users/Lucas/Desktop/consultorBI/DataCriollaGrupo1/streamlit_app/datasets/olist_customers_dataset.csv")
+#print(df_customers.info())
+
+""" 
+ 0   customer_id               99441 non-null  object
+ 3   customer_city             99441 non-null  object
+ 4   customer_state            99441 non-null  object
+"""
+
+df_merged = pd.merge(df_customers, df_orders, on="customer_id")
+#print(df_merged.info())
+
+df_final = pd.merge(df_merged, df_orders_items, on="order_id")
+#print(df_final.info())
+
+df_final_clean = df_final.drop(columns=["customer_zip_code_prefix", "order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "shipping_limit_date"])
+#print(df_final_clean.info())
+#print(df_final["order_approved_at"].value_counts())
+df_final_clean.order_purchase_timestamp = pd.to_datetime(df_final_clean.order_purchase_timestamp)
+#print(df_final_clean.columns)
+#df_final_clean = df_final_clean.set_index("order_purchase_timestamp")
+#print(df_final_clean.columns)
+#df_final_clean.index = df_final_clean.index.to_period('D')
+#print(df_final_clean.head())
+
+
+#print(df_final_clean["order_status"].value_counts())
+df_ts = df_final_clean[['order_purchase_timestamp', 'price']]
+df_ts.dropna(inplace=True)
+df_ts.reset_index(inplace=True)
+df_ts.sort_values(by=['order_purchase_timestamp'], inplace=True, ignore_index=True)
+df_ts2 = df_ts.drop(columns=["index"])
+df_ts2.set_index('order_purchase_timestamp', inplace = True)
+df_d = df_ts2.resample('D').sum()
+df_d['y_mean'] = df_ts2.resample('D').mean()
+df_d['y_operations'] = df_ts2.resample('D').count()
+df_d.isna().sum()
+# Se arma un dataset df_d_na para mostrar los lugares donde hay nulos
+df_d_na = df_d.copy()
+df_d_na['y_na'] = np.nan
+df_d_na['y_na'] = df_d_na['price'].where(df_d_na['price'].notna(),other=0)
+df_d_na['y_na'] = df_d_na['y_na'].where(df_d_na['y_na']==0,other=np.nan)
+# Tendencias sobre el total de observaciones de la serie
+trace1 = go.Scatter(x=df_d_na.index, y=df_d_na['price'],mode='markers', name='Ventas diarias', marker=dict(color='green'))
+trace2 = go.Scatter(x=df_d_na.index, y=df_d_na['y_na'],mode='markers', name='Nulos')
+# Create the figure with both scatter plots
+fig = go.Figure(data=[trace1,trace2])
+fig.update_layout( 
+                  title=dict(text="Venta diaria", font=dict(size=24)), 
+                  xaxis_title='Fecha', 
+                  yaxis_title='Ventas')
+# Show the figure
+#fig.show() descomentar para grafico
+
+
+df_d.notna().sum()
+df_ts.index.nunique()
+
+df_d.shape
+df_d.isna().sum()
+##############################################################################
+#reestructuracion del dataset
+df_d = df_d.loc['2017-01-01':'2018-08-31'].interpolate()
+
+# Tendencias sobre el total de observaciones de la serie
+trace1 = go.Scatter(x=df_d.index, y=df_d['price'],mode='lines', name='Valor de venta diaria')#, marker=dict(color='green'))
+# Create the figure with both scatter plots
+fig = go.Figure(data=[trace1])
+fig.update_layout( 
+                  title=dict(text="Venta diaria por fecha", font=dict(size=24)), 
+                  xaxis_title='Fecha', 
+                  yaxis_title='Ventas')
+# Show the figure
+#fig.show()
+
+##############################################################################
+# Tendencias sobre el total de observaciones de la serie
+trace1 = go.Scatter(x=df_d.index, y=df_d['y_mean'],mode='lines', name='Valor de venta diaria',line=dict(color='green'))#, marker=dict(color='green'))
+# Create the figure with both scatter plots
+fig = go.Figure(data=[trace1])
+fig.update_layout( 
+                  title=dict(text="Valor promedio de venta", font=dict(size=24)), 
+                  xaxis_title='Fecha', 
+                  yaxis_title='Venta')
+# Show the figure
+#fig.show()
+
+##############################################################################
+# Tendencias sobre el total de observaciones de la serie
+trace1 = go.Scatter(x=df_d.index, y=df_d['y_operations'],mode='lines', name='Cantidad de operaciones',line=dict(color='red'))#, marker=dict(color='green'))
+# Create the figure with both scatter plots
+fig = go.Figure(data=[trace1])
+fig.update_layout( 
+                  title=dict(text="Operaciones diarias", font=dict(size=24)), 
+                  xaxis_title='Fecha', 
+                  yaxis_title='Venta')
+# Show the figure
+#fig.show()
+
+##############################################################################
+
+fig = go.Figure(data=go.Box(x=df_d.index.day_name(),y=df_d.price))
+fig.update_layout(title='Estacionalidad semanal', xaxis_title='Día de la semana', yaxis_title='Ventas')
+#fig.show()
+
+
+##############################################################################
+#Tendencia
+df_w = df_d.resample('W',convention='start').sum()
+df_m = df_d.resample('M',convention='start').sum()
+
+
+# Tendencias sobre el total de observaciones de la serie
+trace1 = go.Scatter(x=df_d.index, y=df_d['price'], mode='lines', name='Diaria', fillcolor='blue')
+trace2 = go.Scatter(x=df_w.index, y=df_w['price'], mode='lines', name='Semanal', fillcolor='red')
+trace3 = go.Scatter(x=df_m.index, y=df_m['price'], mode='lines', name='Mensual', fillcolor='green')
+# Create the figure with both scatter plots
+fig = go.Figure(data=[trace1, trace2,trace3])
+fig.update_layout(title='Tendencia de ventas', xaxis_title='Fecha', yaxis_title='Ventas')
+# Show the figure
+#fig.show()
+
+
+# Instanciamos y entrenamos el modelo
+df_ts3 = df_ts.drop(columns=["index"])
+df_ts3.rename(columns={'order_purchase_timestamp':'ds','price':'y'}, inplace=True)
+md = Prophet()
+#md.add_seasonality(name='monthly', period=30.5, fourier_order=5)
+md.fit(df_ts3)
+
+# Prediccion
+future_d = md.make_future_dataframe(periods=180)
+forecast_d = md.predict(future_d)
+print(forecast_d[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+
+""" fig = md.plot(forecast_d)
+a = add_changepoints_to_plot(fig.gca(), md, forecast_d) """
+
+
+""" fig = md.plot_components(forecast_d) """
+
